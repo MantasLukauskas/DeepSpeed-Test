33ae668c2f3fb25a44bb96945804d948409a8a7f
Mantas Lukauskas <52700341+MantasLukauskas@users.noreply.github.com>
2021-10-14 18:28:22 +0300

    Update text.py

--- text.py
+++ text.py
@@ -10,26 +10,31 @@ print("Generator model loaded")
 texts = []
 labels = []
 
-for i in range(1,2):
+for i in range(1):
   print(f"Start of {i} cycle")
-  for j in [200, 300, 400, 500, 600, 700]:
+  for j in [200]:
     for topic in ["basketball","boxing"]:
       if topic == "basketball":
-        keywords = ["About basketball:", "About basketball ball", "basketball", "basketball player", "basketball game", "basketball history"]
+        keywords = ["About basketball:", "About basketball ball:", "About basketball player:", "basketball", "basketball player", "basketball game", "basketball history"]
       if topic == "boxing":
-        keywords = ["About boxing:", "About boxing gloves", "boxing", "boxing champion", "boxing match", "boxing history"]
+        keywords = ["About boxing:", "About boxing gloves:", "About boxing champion", "boxing", "boxing champion", "boxing match", "boxing history"]
       print("Keywords loaded")
       for start in keywords:
         print(start)
         try:
-          texts.append(generator(f"About {start}:",
+          text = generator(f"About {start}:",
                                  do_sample=True,
                                  max_length=j,
                                  num_return_sequences=2,
                                  temperature=random.uniform(0.95, 1.15),
                                  top_k=random.randint(100, 1000),
                                  top_p=random.uniform(0.95, 1.0),
-                                 repetition_penalty=1.0))
+                                 repetition_penalty=1.0)
+          
+          texts.append(text[0]["generated_text"])
+          texts.append(text[1]["generated_text"])
+          
+          labels.append(topic)
           labels.append(topic)
         except:
           print("Error in generation. Skip")
@@ -40,10 +45,10 @@ for i in range(1,2):
 
 textfile = open("generated.txt", "w")
 for element in texts:
-    textfile.write(element + "\n")
+    textfile.write(str(element) + "\n")
 textfile.close()
 
 labelsfile = open("labels.txt", "w")
 for element in labels:
-    labelsfile.write(element + "\n")
+    labelsfile.write(str(element) + "\n")
 labelsfile.close()
