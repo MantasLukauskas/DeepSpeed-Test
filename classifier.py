ab289cd1f45c71c2692f6dbbd56a85cca4f97fbc
MantasLukauskas <mantas.lukauskas@ktu.edu>
2021-10-22 11:58:36 +0300

    Classifier update with printing the number of training and testing obs and labels

--- classifier.py
+++ classifier.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import re
 import argparse
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from transformers import DistilBertTokenizerFast
+import torch
+from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+import numpy as np
+from sklearn.metrics import accuracy_score
 
 def main():
     # Create the parser
@@ -9,7 +16,6 @@ def main():
     parser.add_argument('--test_file', type=str, help='Input dir for videos')
     parser.add_argument('--batch_size', type=int, help='Input dir for videos')
     args = parser.parse_args()
-
 
     train = pd.read_excel(args.train_file)
     print(f"Training file loaded: {args.train_file}")
@@ -23,31 +29,25 @@ def main():
     train['input'] = train['input'].str.replace('\n', '')
     train['input'] = train['input'].str.replace(':', '')
     train['input'] = train['input'].str.replace(';', '')
-
     train['input'] = train['input'].apply(lambda row: re.sub(r"[^a-zA-Z0-9]", " ", str(row)))
-
     train["len"] = train.apply(lambda row: len(row["input"]), axis=1)
-
     train = train[train["len"] > 20]
-
     train = train.dropna()
 
     test['input'] = test['input'].str.replace('\n', '')
     test['input'] = test['input'].str.replace(':', '')
     test['input'] = test['input'].str.replace(';', '')
-
     test['input'] = test['input'].apply(lambda row: re.sub(r"[^a-zA-Z0-9]", " ", str(row)))
-
     test["len"] = test.apply(lambda row: len(row["input"]), axis=1)
-
     test = test[test["len"] > 20]
 
+    print(f"Length of training dataset after preprocessing is {len(train)}")
+    print(f"Length of testing dataset after preprocessing is {len(test)}")
 
     print("Text preprocessing is done")
 
     train_texts = train["input"].to_list()
 
-    from sklearn import preprocessing
     le = preprocessing.LabelEncoder()
     le.fit(train["label"])
 
@@ -58,17 +58,15 @@ def main():
     test["label"] = le.transform(test["label"])
     test_labels = test["label"].to_list()
 
-    from sklearn.model_selection import train_test_split
+    print(f"Number of label is {len(train['label'].unique())}")
+
     train_texts, val_texts, train_labels, val_labels = train_test_split(train_texts, train_labels, test_size=.2)
 
-    from transformers import DistilBertTokenizerFast
     tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 
     train_encodings = tokenizer(train_texts, truncation=True, padding=True)
     val_encodings = tokenizer(val_texts, truncation=True, padding=True)
     test_encodings = tokenizer(test_texts, truncation=True, padding=True)
-
-    import torch
 
     class IMDbDataset(torch.utils.data.Dataset):
         def __init__(self, encodings, labels):
@@ -86,8 +84,6 @@ def main():
     train_dataset = IMDbDataset(train_encodings, train_labels)
     val_dataset = IMDbDataset(val_encodings, val_labels)
     test_dataset = IMDbDataset(test_encodings, test_labels)
-
-    from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
 
     training_args = TrainingArguments(
         output_dir='./results',  # output directory
@@ -110,22 +106,16 @@ def main():
         eval_dataset=val_dataset  # evaluation dataset
     )
 
-
     trainer.train()
 
     prediction = trainer.predict(test_dataset)
 
-    import numpy as np
     unique, counts = np.unique(prediction.label_ids, return_counts=True)
     dict(zip(unique, counts))
-
     unique, counts = np.unique(np.array(test_labels), return_counts=True)
     dict(zip(unique, counts))
 
-    from sklearn.metrics import accuracy_score
-    print("Accuracy is ",accuracy_score(prediction.label_ids, test_labels))
-
-
+    print("Accuracy is ", accuracy_score(prediction.label_ids, test_labels))
 
 if __name__ == '__main__':
     main()
