b2abb0a14faa7b729ca839ceae34883cac7bdf7f
MantasLukauskas <mantas.lukauskas@ktu.edu>
2021-10-28 22:38:11 +0300

    Change of the tokenizer and classificator to BERT instead of RoBERTa

--- classifier.py
+++ classifier.py
@@ -102,7 +102,7 @@ def main():
 
     training_args = TrainingArguments(
         output_dir='./results_multi_roberta',  # output directory
-        num_train_epochs=1,  # total number of training epochs
+        num_train_epochs=3,  # total number of training epochs
         per_device_train_batch_size=args.batch_size,  # batch size per device during training
         per_device_eval_batch_size=args.batch_size,  # batch size for evaluation
         warmup_steps=100,  # number of warmup steps for learning rate scheduler
@@ -136,7 +136,7 @@ def main():
         eval_dataset=val_dataset  # evaluation dataset
     )
 
-    trainer.train()
+    trainer.train("results_multi_roberta/checkpoint-20000")
 
     import numpy as np
     from sklearn.metrics import accuracy_score
@@ -174,7 +174,7 @@ def main():
 
     pred_labels = le.inverse_transform(preds)
 
-    with open('predictions_multi_roberta.txt', 'w') as f:
+    with open('predictions_multi_bert.txt', 'w') as f:
         for item in pred_labels:
             f.write("%s\n" % item)
 
