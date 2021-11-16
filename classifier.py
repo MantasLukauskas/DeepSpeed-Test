4096d69244d778257550f5f41d18ed02ce819fe8
MantasLukauskas <mantas.lukauskas@ktu.edu>
2021-11-16 09:03:26 +0200

    Classifier update with printing the number of training and testing obs and labels

--- classifier.py
+++ classifier.py
@@ -69,7 +69,7 @@ def main():
 
     # tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased-finetuned-sst-2-english')
     # tokenizer = RobertaTokenizer.from_pretrained('roberta-large-mnli')
-    tokenizer = BertTokenizer.from_pretrained('./results_new_multi/checkpoint-75000')
+    tokenizer = BertTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
     # tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-125M')
     # tokenizer.pad_token = tokenizer.eos_token
 
@@ -101,7 +101,7 @@ def main():
 
     training_args = TrainingArguments(
         output_dir='./results_new_multi_more_epochs',  # output directory
-        num_train_epochs=2,  # total number of training epochs
+        num_train_epochs=3,  # total number of training epochs
         per_device_train_batch_size=args.batch_size,  # batch size per device during training
         per_device_eval_batch_size=args.batch_size,  # batch size for evaluation
         warmup_steps=100,  # number of warmup steps for learning rate scheduler
